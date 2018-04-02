98c5cf559a5c70c9dec07bf3629f6d69beebac20
Noel Waghorn <noel@adapptor.com.au>
2018-04-02 22:53:25 +0800

    The impossible bet... solved?
    Start by searching for the dollar bill in the box at index of the
    person's place in line (minus one because arrays are 0 indexed).
    If you didn't find it, try searching for the index of the dollar bill
    you took out of the box. Do this within 50 tries for each person.

---
+++ gamble.hs
@@ -0,0 +1,30 @@
+module Main where
+
+import System.Random
+import Control.Lens
+
+shuffle :: [a] -> IO [a]
+shuffle [] = return []
+shuffle xs = do
+  randomPosition <- getStdRandom (randomR (0, length xs - 1))
+  let (left, (a:right)) = splitAt randomPosition xs
+  fmap (a:) (shuffle (left ++ right))
+
+finMyNumByIndex :: (Traversable t, Ord t1, Num t1) => Int -> Int -> t Int -> t1 -> Bool
+finMyNumByIndex index target listOfInts attempts =
+  if attempts > 50 then False
+  else
+    if listOfInts ^?! element (index - 1) == target then True
+    else finMyNumByIndex (listOfInts ^?! element (index - 1)) target listOfInts (attempts + 1)
+
+allFound :: (Traversable t, Ord t1, Num t1) => Int -> t Int -> Bool
+allFound target listOfInts =
+  if target > 100 then True
+  else
+    if finMyNumByIndex target target listOfInts 1 then allFound (target + 1) listOfInts
+    else False
+
+main = do
+  x <- shuffle [1..100]
+  let c =  allFound 1 x
+  print c
